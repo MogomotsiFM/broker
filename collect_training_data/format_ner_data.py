018c2402c67b128c1492caf8d8ffefb046fdd479
MogomotsiFM <adviceseiphemo@yahoo.co.uk>
2022-11-27 21:58:46 +0200

    Anotated the contact numbers. This seems to be useful for service providers.

--- collect_training_data/format_ner_data.py
+++ collect_training_data/format_ner_data.py
@@ -30,11 +30,16 @@ def create_price_re():
 
 def create_property_type_re():
     bach   = r'bachelor(\srooms?)?'
-    house1 = r'[0-9]+\s*bed\s*rooms*(\shouse)?'
+    house1 = r'[0-9]+(\s?or\s?[0-9])?\s*bed\s*rooms*(\shouse)?'
     house2 = r'[0-9](\s?or\s?[0-9])?\s?roomed[-\s]house'
     room   = r'\b((1|one|single)\s?)?rooms?'
 
     return re.compile(bach + '|' + house1 + '|' + house2 + '|' + room + '|house')
+
+
+def create_contact_type_re():
+    # Notice the hack to ensure that the last character is a number and not a space.
+    return re.compile(r'(0|\+?[0-9]{1,3})\s?[0-9\s]{8,10}[0-9]')
 
 
 def find_start(processed_post):
@@ -55,10 +60,11 @@ def find_location_name(processed_post, area_names):
         w1 = processed_post[start1:start2-1]
         w2 = processed_post[start2:end-1]
         if not w1.isupper() and len(w1) + len(w2) > 3:
-            _, prob = process.extractOne(w1, area_names, scorer=fuzz.ratio)
+            word, prob = process.extractOne(w1, area_names, scorer=fuzz.ratio)
 
-            _, prob2 = process.extractOne(' '.join([w1, w2]), area_names, scorer=fuzz.token_sort_ratio)
+            word2, prob2 = process.extractOne(' '.join([w1, w2]), area_names, scorer=fuzz.token_sort_ratio)
 
+            print(w1, ' : ', w2, ' : ', word, ' : ', word2, ' : ', prob, ' : ', prob2)
             if prob2 > prob and prob2 > 90:
                 new_bounds = (start1, end-1, prob2)
                 settle_possible_overlap(area_names_boundaries, new_bounds)
@@ -117,8 +123,10 @@ def preprocess(post):
 
 
 def expand_units(processed_post: str):
+    separators = r'(,|/|or)'
     # Mmabatho units re: unit 1, 2, 3 or 6
-    units_re = re.compile(r'unit\s?[0-9]{1,2}(\s?[,/]\s?[0-9]{1,2})*(\s?or\s?[0-9]{1,2})?')
+    # units_re = re.compile(r'unit\s?[0-9]{1,2}(\s?[,/]\s?[0-9]{1,2})*(\s?or\s?[0-9]{1,2})?')
+    units_re = re.compile(r'unit\s?[0-9]{1,2}(\s?(,|/|or)\s?[0-9]{1,2})*')
 
     # unit 1,2,3 or 6 
     units_match = units_re.search(processed_post)
@@ -126,7 +134,7 @@ def expand_units(processed_post: str):
         # Split on the separator
         raw_units = processed_post[units_match.start():units_match.end()]
 
-        units, _ = re.compile(r'[,/(or)]').subn(lambda m: ' ', raw_units)
+        units, _ = re.compile(separators).subn(lambda m: ' ', raw_units)
         units = units.split()
 
         areas = ''
@@ -148,8 +156,9 @@ def near(area):
     """
     return [a+f'{i}' for i, a in enumerate(area) if a != ' ']
 
+
 def process_surounding_areas(processed_post):
-    near_re = re.compile(r'near|around|(close to)')
+    near_re = re.compile(r'near|around|(close to)|(walking distance to)')
 
     seen = near_re.search(processed_post)
 
@@ -169,6 +178,7 @@ area_names = get_area_names()
 # Property types
 property_re = create_property_type_re()
 price_re    = create_price_re()
+contacts_re = create_contact_type_re()
 
 ss = 'single room available to rent next to the ext 39 for r1.5k call or watsapp 0734872827'
 print(property_re.search(ss))
@@ -191,6 +201,7 @@ print(expand_units(us2))
 ss = 'Bachelor rooms for rent ( Signalhill- Mafhikeng ) Rent; R1,800.00 Watsup: 0633312221'
 # a  = 'P_TYPE for rent ( Signalhill- Mafhikeng ) Rent; P_PRICE Watsup: 0633312221'
 # b  = '0      7   11  16 19         30        39 41   47      55       63         73'
+ss = "Hello I'm Looking For A Room To Rent Preferably At Imperial Reserve Or Golf View. My Budget Is R1500 to R2000"
 w, p = process.extractOne(ss, area_names)
 print(ss)
 processed_post = preprocess(ss)
@@ -202,23 +213,23 @@ print('Here\n')
 processed_post = replace_location_with_ner_marker(processed_post, area_names)
 print(processed_post)
 
+print('\n\nHere')
+processed_post = replace_location_with_ner_marker("hi i'm looking 4 a room mo majemantsho", area_names)
+print(processed_post)
 
-print('There:   ', re.compile(r'\b([1(one)(single)]\s?)?rooms?').search('rooms '))
-
-# print(units_re.search('Looking for a place to rent in unit 1'))
-# print(units_re.search('Looking for a place to rent in unit 1,2, 3'))
 
 with open('training_data.txt', 'r', encoding='utf-16le') as file:
     posts = file.readlines()
 
 i = 0
 with open('labeled_ner_training_data.txt', 'w', encoding='utf-16le') as file:
-    for post in [p for p in posts if len(p) > 2]:
+    for post in [p for p in posts[0:1] if len(p) > 2]:
         print(f'    {i}')
         i += 1
         processed_post    = preprocess(post)
         processed_post, _ = property_re.subn('P_TYPE', processed_post, 100)
         processed_post, _ = price_re.subn('P_PRICE', processed_post, 100)
+        processed_post, _ = contacts_re.subn('P_TELL', processed_post, 100)
 
         processed_post = expand_units(processed_post)
         processed_post, _ = re.compile(r',').subn(lambda m: ' , ', processed_post)
