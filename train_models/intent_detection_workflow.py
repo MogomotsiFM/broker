72c355e2efd87c53f46e15a088f5d67bf4387679
Mog <amg.seiphemo@gmail.com>
2023-09-27 22:04:16 +0200

    Added the requirements file and fixed a number of bugs. How was this working before with all those issues.

--- train_models/intent_detection_workflow.py
+++ train_models/intent_detection_workflow.py
@@ -1,17 +1,20 @@
 import os
 
-import gdown
-#import shutil
+import emoji
+
+# import gdown
+# import shutil
 import pandas as pd
 import numpy as np
 
 import tensorflow as tf
 import tensorflow_hub as hub
-import tensorflow_text as text
-import seaborn as sns
-from pylab import rcParams
+import tensorflow_text
 
-import matplotlib.pyplot as plt
+import matplotlib
+import seaborn
+
+from pylab import rcParams
 
 from sklearn.preprocessing import LabelBinarizer
 
@@ -19,13 +22,25 @@ from bert_model_urls import bert_model_urls
 from plot_results import plot_results
 
 
+def clean_data(data):
+    corrupted_data = data[ pd.isna(data['posts']) ]
+    print("Number of corrupted data points: ", corrupted_data.shape)
+    corrupted_data.to_csv("../data/corrupted_data.csv")
+    data = data[ pd.notna(data['posts']) ]
+    data['posts'] = data['posts'].map( 
+            lambda post: emoji.replace_emoji(post, replace='')
+        )
+
+    return data
+
 def preprocess(data, binarizer):
     features = data.copy()
-    labels   = features.pop('intent')
-    # trainlabels   = traindf['intent']
-    features = features.values
-    labels = binarizer.fit_transform(labels.values)
-
+    labels   = features.pop('label')
+    print(f"Some of the labels: {labels[0:5]}")
+    # features = features['posts']
+    features = features.pop('posts')
+    labels = binarizer.transform(labels.values)
+    print(f"Some of the binarized labels: {labels[0:5]}")
     return features, labels
 
 
@@ -44,9 +59,9 @@ def make_dir(path):
 
 def build_classifier_model(tfhub_handle_preprocess, tfhub_handle_encoder):
     encoder = [c for c in tfhub_handle_encoder if c not in ('\\/:')]
-    root = 'model\\BERT'
+    root = os.path.join('model', 'BERT')
     model_name = ''.join(encoder)
-    model_path = root + '\\' + model_name
+    model_path = os.path.join(root, model_name)
 
     if not os.path.exists(model_path):
 
@@ -62,7 +77,7 @@ def build_classifier_model(tfhub_handle_preprocess, tfhub_handle_encoder):
 
         net = outputs['pooled_output']
         net = tf.keras.layers.Dropout(0.1)(net)
-        net = tf.keras.layers.Dense(7, activation=None, name='classifier')(net)
+        net = tf.keras.layers.Dense(3, activation=None, name='classifier')(net)
 
         model = tf.keras.Model(text_input, net)
 
@@ -74,18 +89,20 @@ def build_classifier_model(tfhub_handle_preprocess, tfhub_handle_encoder):
     print('\nReading BERT model from file.\n')
     return tf.keras.models.load_model(model_path)
 
+
 def split_data_helper(data, train_data_ratio):
+    # Shuffle
     data = data.sample(frac=1.0)
 
     num_samples, _ = data.shape
 
     num_train_samples     = int(train_data_ratio * num_samples)
-    num_validate_samples = int( ((1.0 - train_data_ratio) / 2) * num_train_samples )
+    num_validate_samples = int( ( num_samples - num_train_samples ) / 2 )
 
-    train_samples = data[:, 0:num_train_samples]
+    train_samples = data.iloc[0:num_train_samples, :]
     validate_upper = num_train_samples + num_validate_samples
-    validate_samples = data[:, num_train_samples:validate_upper]
-    test_samples = data[:, validate_upper:]
+    validate_samples = data.iloc[num_train_samples:validate_upper, :]
+    test_samples = data.iloc[validate_upper:, :]
 
     return train_samples, validate_samples, test_samples
 
@@ -104,20 +121,37 @@ def split_data(data, train_data_ratio):
     # Ensure that all classes are equally represented during training
 
     # Combine the data into test, validation and test data
-    train_data    = pd.DataFrame([train_tenants, train_landlords, train_irrelevant])
-    validate_data = pd.DataFrame([validate_tenants, validate_landlords, validate_irrelevant])
-    test_data     = pd.DataFrame([test_tenants, test_landlords, test_irrelevant])
+    train_data    = pd.concat([train_tenants, train_landlords, train_irrelevant])
+    validate_data = pd.concat([validate_tenants, validate_landlords, validate_irrelevant])
+    test_data     = pd.concat([test_tenants, test_landlords, test_irrelevant])
 
     train_data = train_data.sample(frac=1.0)
 
     return train_data, validate_data, test_data
 
 
+def debug_data(features, labels):
+    try:
+        loss, accuracy = classifier_model.evaluate(features, labels)
+        print(f'Loss: {loss}')
+        print(f'Accuracy: {accuracy}\n')
+    except:
+        for f in features:
+            try:
+                a = []
+                a.append(f)
+                classifier_model.predict(tf.constant(a))
+            except Exception as e:
+                print('Errored out')
+                print(f)
+                raise e
+
+
 tf.get_logger().setLevel('ERROR')
 
-sns.set(style='whitegrid', palette='muted', font_scale=1.2)
+seaborn.set(style='whitegrid', palette='muted', font_scale=1.2)
 HAPPY_COLORS_PALETTE = ['#01BEFE', '#FFDD00', '#FF7D00', '#FF006D', '#ADFF02', '#8F00FF']
-sns.set_palette(sns.color_palette(HAPPY_COLORS_PALETTE))
+seaborn.set_palette(seaborn.color_palette(HAPPY_COLORS_PALETTE))
 rcParams['figure.figsize'] = 12, 8
 
 
@@ -126,17 +160,22 @@ warnings.filterwarnings('ignore')
 
 
 # LOAD DATA
-DATA_PATH = '..\\data\\labeled_intent_detection_dataset.csv'
+# DATA_PATH = '..\\data\\labeled_intent_detection_dataset.csv'
+DATA_PATH = os.path.join('..', 'data', 'labeled_intent_detection_dataset.csv')
 data = pd.read_csv(DATA_PATH)
 
+data = clean_data(data)
+
+print('All the data as loaded: ', data.shape)
 traindf, validdf, testdf = split_data(data, 0.7)
 
-chart = sns.countplot(x=traindf['intent'], palette=HAPPY_COLORS_PALETTE)
-plt.title('Number of texts per intent')
+chart = seaborn.countplot(x=traindf['label'], palette=HAPPY_COLORS_PALETTE)
+chart.set_title("Number of texts per intent")
+#plt.title('Number of texts per intent')
 chart.set_xticklabels(chart.get_xticklabels(), rotation=30, horizontalalignment='right')
 # plt.waitforbuttonpress()
 
-binarizer = LabelBinarizer()
+binarizer = LabelBinarizer().fit(traindf["label"])
 train_features, train_labels = preprocess(traindf, binarizer)
 test_features, test_labels   = preprocess(testdf, binarizer)
 validate_features, validate_labels = preprocess(validdf, binarizer)
@@ -147,14 +186,15 @@ tfhub_handle_encoder, tfhub_handle_preprocess = bert_model_urls(model_name)
 print(f'BERT model selected: {tfhub_handle_encoder}')
 print(f'Preprocess model auto-selected: {tfhub_handle_preprocess}')
 
-trained_model_dir = 'model/fine_tuned'
+# trained_model_dir = 'model/fine_tuned'
+trained_model_dir = os.path.join('model', 'fine_tuned')
 
 retrain = False
 fine_tuned_model_name = model_name.replace('/', '-')
 fine_tuned_model_name = fine_tuned_model_name.replace('\\', '-')
 
-trained_model_path = trained_model_dir + '\\' + fine_tuned_model_name
-
+# trained_model_path = trained_model_dir + '\\' + fine_tuned_model_name
+trained_model_path = os.path.join(trained_model_dir, fine_tuned_model_name)
 # DEFINE THE MODEL: Preprocessing layer, BERT model, dense layer, dropout layer
 if retrain or not os.path.exists(trained_model_path):
     print('\nRe-training a fresh model\n')
@@ -163,8 +203,17 @@ else:
     print('\nLoading a pre-trained model\n')
     classifier_model = tf.keras.models.load_model(trained_model_path)
 
-bert_raw_result = classifier_model(tf.constant(train_features[0]))
+print(f"Model input shape: {train_features.shape}")
+print(f"Some of the data: {train_features[0:5]}")
+print(f"One sample: {train_features.iloc[0]}")
+# bert_raw_result = classifier_model(tf.constant(train_features[0]))
+input = ['any available room near town or in town...atleast where i can take walk from where i stay to town']
+bert_raw_result  = classifier_model(tf.constant(input))
+bert_raw_result2 = classifier_model.predict(tf.constant(input))
 print(tf.keras.activations.softmax(bert_raw_result))
+print('\n')
+print(binarizer.inverse_transform(bert_raw_result.numpy()))
+print(tf.sigmoid(bert_raw_result2))
 
 # TRAIN THE MODEL
 history = None
@@ -195,7 +244,7 @@ if retrain or not os.path.exists(trained_model_path):
 
 # EVALUATE THE MODEL
 if retrain:
-    print('\nUsing validation data to evaluating a trained model')
+    print('\nUsing test data to evaluating a trained model')
     loss, accuracy = classifier_model.evaluate(test_features, test_labels)
     print(f'Loss: {loss}')
     print(f'Accuracy: {accuracy}\n')
@@ -207,20 +256,17 @@ else:
     # labels   = np.concatenate((test_labels, train_labels, validate_features))
 
     print('\nEvaluate the trained model')
-    loss, accuracy = classifier_model.evaluate(train_features, train_labels)
-    print('Training')
-    print(f'Loss: {loss}')
-    print(f'Accuracy: {accuracy}\n')
+    print(f'Train labels: {train_labels[0:5]}')
+    print(f'Train labels type: {train_labels.dtype}, shape: {train_labels.shape}')
+    print(f'Train input type: {train_features.dtype}, shape: {train_features.shape}')
+    # loss, accuracy = classifier_model.evaluate(train_features, train_labels)
+    debug_data(train_features, train_labels)
 
-    loss, accuracy = classifier_model.evaluate(test_features, train_labels)
-    print('Test')
-    print(f'Loss: {loss}')
-    print(f'Accuracy: {accuracy}\n')
+    # loss, accuracy = classifier_model.evaluate(test_features, test_labels)
+    debug_data(test_features, test_labels)
 
-    loss, accuracy = classifier_model.evaluate(validate_features, validate_labels)
-    print('Validate')
-    print(f'Loss: {loss}')
-    print(f'Accuracy: {accuracy}\n')
+    # loss, accuracy = classifier_model.evaluate(validate_features, validate_labels)
+    debug_data(validate_features, validate_labels)
 
 # PLOT ACCURACY AND LOSS OVER TIME
 if history is not None:
